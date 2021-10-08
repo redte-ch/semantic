2605e3290e91c51a7c7fa322d025f6143e3c07e3
Mauko Quiroga <mauko@pm.me>
2021-10-08 20:08:16 +0200

    Add a session cache for lock & toml files

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 .DEFAULT_GOAL := test
 
+install:
+	@pip install --upgrade pip wheel setuptools
+	@pip install --upgrade poetry
+
 compile: src
 	@poetry run python -m compileall -q $?
 
@@ -9,16 +13,9 @@ clean: src
 test: compile clean
 	@poetry run pytest -qx
 
-test-nox: \
-	test-nox-3.7.11 \
-	test-nox-3.8.12 \
-	test-nox-3.9.7 \
-	test-nox-3.10.0 \
-	;
+test-nox:
+	@poetry run nox -s test
 
-test-nox-%:
-	@poetry run nox -p $*
-
-version:
+version: test-nox
 	@git tag ${version}
 	@git push --tags
--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,58 @@
-import os
 import pathlib
+import re
 import shutil
-import tempfile
+from typing import Any, Pattern
 
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
-py_versions = ["3.7.11", "3.8.12", "3.9.7", "3.10.0"]
-np_versions = ["1.18.5", "1.19.5", "1.20.3", "1.21.2"]
+python_versions = ("3.7.12", "3.8.12", "3.9.7")
+numpy_versions = ("1.17.5", "1.18.5", "1.19.5", "1.20.3", "1.21.2")
+exclude = (("3.9.7", "1.18.5"),)
+matrix = (
+    (python, numpy)
+    for python in python_versions
+    for numpy in numpy_versions
+    if (python, numpy) not in exclude
+    )
 
 
-@nox.session(python = py_versions)
-@nox.parametrize("numpy", np_versions, ids = np_versions)
+class SessionCache:
+    sesh_name: str
+    sesh_cache: str
+    pattern: Pattern = re.compile(r"\(|\)|\=|\'|\"|\,|\s")
+
+    def __init__(self, sesh_name: str, sesh_cache: str) -> None:
+        self.lock_name = self.pattern.sub("-", sesh_name)
+        self.back_lock = pathlib.Path(f"{sesh_cache}/poetry.lock")
+        self.back_toml = pathlib.Path(f"{sesh_cache}/pyproject.toml")
+        self.base_lock = pathlib.Path("poetry.lock")
+        self.base_toml = pathlib.Path("pyproject.toml")
+        self.sesh_lock = pathlib.Path(f"{sesh_cache}/lock-{self.lock_name}")
+
+        if not self.sesh_lock.exists():
+            shutil.copyfile(self.base_lock, self.sesh_lock)
+
+    def __enter__(self) -> None:
+        shutil.copyfile(self.base_lock, self.back_lock)
+        shutil.copyfile(self.base_toml, self.back_toml)
+        shutil.copyfile(self.sesh_lock, self.base_lock)
+
+    def __exit__(self, *__: Any) -> None:
+        shutil.copyfile(self.base_lock, self.sesh_lock)
+        shutil.copyfile(self.back_lock, self.base_lock)
+        shutil.copyfile(self.back_toml, self.base_toml)
+        self.back_lock.unlink()
+        self.back_toml.unlink()
+
+
+@nox.session
+@nox.parametrize("python, numpy", matrix)
 def test(session, numpy):
-    with tempfile.TemporaryDirectory() as test_dir:
-        base_dir = pathlib.Path(os.path.abspath("."))
-        shutil.rmtree(test_dir)
-        shutil.copytree(base_dir, test_dir)
-        session.chdir(test_dir)
-        session.install("poetry")
-        session.run("poetry", "add", f"numpy@{numpy}")
-        session.run("poetry", "install")
-        session.run("poetry", "run", "pytest", "-qx")
+    session.run("make", "install", external = True, silent = True)
+
+    with SessionCache(session.name, session.cache_dir):
+        session.run("poetry", "add", f"numpy@{numpy}", silent = True)
+        session.run("poetry", "install", silent = True)
+
+    session.run("poetry", "run", "pytest", "-qx")
