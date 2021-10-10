84f987df2554c93cf86fcccc0f0fa8f9aa58c613
Mauko Quiroga <mauko@pm.me>
2021-10-10 23:34:58 +0200

    Improve doc metadata

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,16 @@
+# Copyleft (ɔ) 2021 Mauko Quiroga <mauko@pm.me>
+#
+# Licensed under the EUPL-1.2-or-later
+# For details: https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+
 """Sphinx configuration."""
 
-author = "Mauko Quiroga"
+author = "Mauko Quiroga <mauko@pm.me>"
 project = "pysemver"
-extensions = ["sphinx.ext.doctest"]
+copyleft = f"(ɔ) 2021 {author}"
+extensions = (
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    )
